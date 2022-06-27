c14b2ccdb7d08bd834815d79441c825b096e55e8
Emuejevoke Eshemitan <96771321+Davidsonity@users.noreply.github.com>
2022-06-27 21:05:18 +0100

    Update Zomato_app.py

--- app/Zomato_app.py
+++ app/Zomato_app.py
@@ -2,11 +2,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+import os
+
+current_path = os.getcwd()
+model_path = os.path.join(current_path, 'model.pkl')
 
 import pickle
-model = pickle.load(open('/model.pkl','rb'))
+model = pickle.load(open(model_path,'rb'))
 
 import warnings
 warnings.filterwarnings('ignore')
