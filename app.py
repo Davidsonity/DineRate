bdda96d4fd11c8297630c41d6ddb1bf79b7b2003
Emuejevoke Eshemitan <96771321+Davidsonity@users.noreply.github.com>
2022-06-28 02:09:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 model_path = 'model.pkl'
 
 import pickle
-model_1 = pickle.load(open(model_path,'rb'))
+model = pickle.load(open(model_path,'rb'))
 
 import warnings
 warnings.filterwarnings('ignore')
